413e84c13f4bfbbbf614072dd6306ef8b0ed926d
Wilson Lam <bugbug0102@gmail.com>
2017-11-26 22:16:46 +0800

    add sorting feature

--- src/main/scala/org/b0102/spring/boot/pkg/repair/Main.scala
+++ src/main/scala/org/b0102/spring/boot/pkg/repair/Main.scala
@@ -9,9 +9,15 @@ import org.apache.commons.io.IOUtils
 import java.util.zip.ZipOutputStream
 import java.io.FileOutputStream
 import java.util.zip.Deflater
+import java.util.TreeMap
 
 object Main 
 {
+  private class ZipDataBean(val name:String, val size:Long, val crc:Long, val data:Array[Byte] ) extends Serializable with Comparable[ZipDataBean]
+  {
+    private def this() = this(null, 0, 0, null)
+    override def compareTo(zip:ZipDataBean):Int = name.compareTo(zip.name)
+  }
   
   def showHelp():Unit =
   {
@@ -23,7 +29,6 @@ object Main
   
   def main(args:Array[String]):Unit =
   {
-    
     if(args.length >= 2)
     {
       val in = new File(args(0))
@@ -31,41 +36,51 @@ object Main
       
       var fis:FileInputStream = null
       var zis:ZipInputStream  = null
+      val zds = try
+      {
+        fis = new FileInputStream(in)
+        zis = new ZipInputStream(fis)
+        val ret = new collection.mutable.ListBuffer[ZipDataBean]()
+        
+        var ze:ZipEntry = zis.getNextEntry
+        while(ze!=null)
+        {
+          val zd = new ZipDataBean(ze.getName, ze.getSize, ze.getCrc, IOUtils.toByteArray(zis))
+          ze = zis.getNextEntry
+          ret += zd
+        }
+        ret
+      
+      }finally
+      {
+        IOUtils.closeQuietly(zis)
+        IOUtils.closeQuietly(fis)
+        List()
+      }
       
       var fos:FileOutputStream = null
       var zos:ZipOutputStream = null
       try
       {
-        fis = new FileInputStream(in)
-        zis = new ZipInputStream(fis)
-        
         fos = new FileOutputStream(out)
         zos = new ZipOutputStream(fos)
         
-        var ze:ZipEntry = zis.getNextEntry
-        while(ze!=null)
-        {
-          val nze = new ZipEntry(ze.getName)
-          if(ze.getSize > 0)
+        zds.sorted.foreach{ zd=>
+          val nze = new ZipEntry(zd.name)
+          if(zd.size > 0)
           {
             nze.setMethod(ZipOutputStream.STORED)
-            nze.setCrc(ze.getCrc)
-            nze.setSize(ze.getSize)
+            nze.setCrc(zd.crc)
+            nze.setSize(zd.size)
           }
-          println(s"Working:\t ${ze.getName}")
-          
+          println(s"Working:\t ${zd.name}")
           zos.putNextEntry(nze)
-          zos.write(IOUtils.toByteArray(zis))
+          zos.write(zd.data)
           zos.closeEntry()
-          
-          ze = zis.getNextEntry
         }
         
       }finally
       {
-        IOUtils.closeQuietly(zis)
-        IOUtils.closeQuietly(fis)
-        
         IOUtils.closeQuietly(zos)
         IOUtils.closeQuietly(fos)
       }
